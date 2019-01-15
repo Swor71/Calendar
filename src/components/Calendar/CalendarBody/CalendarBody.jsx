b5648b02f1686d28e449f2e81315a35ef152cdf6
Marcin Swornowski <swor71@gmail.com>
2019-01-15 15:32:32 +0100

    Add placeholder view of CalendarBody

--- src/components/Calendar/CalendarBody/CalendarBody.jsx
+++ src/components/Calendar/CalendarBody/CalendarBody.jsx
@@ -1,10 +1,19 @@
 import React, { PureComponent } from 'react';
 
-class CalendarBody extends PureComponent {
-  render() {
-    return (
-      <div>
+import './CalendarBody.css';
 
+class CalendarBody extends PureComponent {
+
+  render() {
+    let daysArr = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+    let arr = [];
+    for (let i = 1; i <= 31; i++) {
+      arr.push(i);
+    }
+    return (
+      <div className='calendar-body-wrapper'>
+        {daysArr.map((day, index) => <div className='weekday' key={index} >{day}</div>)}
+        {arr.map((day, index) => <div className='day' key={index} >{day}</div>)}
       </div>
     );
   }
